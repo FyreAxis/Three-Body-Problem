3ce01f3387247b5111b7397a63b6dcf5e15e6ee2
FyreAxis <xxcomedyskitzxx@gmail.com>
2025-06-23 15:01:45 -0500

    Changes calculations to rely on acceleration due to gravity w/o going through forces first.

--- main.js
+++ main.js
@@ -88,9 +88,9 @@ class Body{
         }
     }
 
-    newAccel(netForce){
-        for (let i = 0; i < 3; i++){ //F = ma => a = F/m
-            this.a[i] = netForce[i] / this.m;
+    newAccel(netAccel){
+        for (let i = 0; i < 3; i++){
+            this.a[i] = netAccel[i];
         }
     }
 
@@ -103,6 +103,37 @@ class Body{
             distance[i] += body2.p[i] - body1.p[i];
         }
         return distance;
+    }
+
+    //This calculates the acceleration due to gravity of the first body on the second body
+    static vectorGravAccel(body1, body2){
+        //A_g = G (m_1) / (r^2)
+        //This calculates what the second body's acceleration due to gravity is
+        const g = 6.67e-11;
+        let gravAccels = [0, 0, 0];
+        let distances = Body.vectorDistance(body1, body2);
+        for (let i = 0; i < 3; i++){
+            //TODO: Case of zero
+            if (distances[i] != 0){
+                gravAccels[i] += g * body1.m;
+                gravAccels[i] /= (distances[i] * distances[i]);
+                //Reverse direction if body2's pos is bigger
+                //TODO: Turn into ternary statement
+                if (body2.p[i] > body1.p[i]){ gravAccels[i] *= -1; }
+            }
+        }
+        return gravAccels;
+    }
+
+    static netAccel(body1, body2, body3){
+        //2 on 1 and 3 on 1
+        let netAccel = [0, 0, 0];
+        let a1 = Body.vectorGravAccel(body2, body1);
+        let a2 = Body.vectorGravAccel(body3, body1);
+        for (let i = 0; i < 3; i++){
+            netAccel[i] += a1[i] + a2[i];
+        }
+        return netAccel;
     }
 
     //This calculates the force of the first body on the second body
@@ -132,17 +163,18 @@ class Body{
         }
         return netForce;
     }
+
     //Resolution is how often calculations are simulated (how accurate)
     //Total duration is how long the simulation goes for
     //These are both in seconds in terms of the simulation, not computation
     static simulate(body1, body2, body3, resolutionTime, totalDuration){
         for (let time = 0; time < totalDuration; time += resolutionTime){
-            let netForceOn1 = Body.netForce(body1, body2, body3);
-            let netForceOn2 = Body.netForce(body2, body3, body1);
-            let netForceOn3 = Body.netForce(body3, body1, body2);
-            body1.newAccel(netForceOn1);
-            body2.newAccel(netForceOn2);
-            body3.newAccel(netForceOn3);
+            let netAccelOn1 = Body.netAccel(body1, body2, body3);
+            let netAccelOn2 = Body.netAccel(body2, body3, body1);
+            let netAccelOn3 = Body.netAccel(body3, body1, body2);
+            body1.newAccel(netAccelOn1);
+            body2.newAccel(netAccelOn2);
+            body3.newAccel(netAccelOn3);
             body1.newVelocity(resolutionTime);
             body2.newVelocity(resolutionTime);
             body3.newVelocity(resolutionTime);
@@ -182,10 +214,6 @@ console.log("Gravitational force of 1 on 3: " + JSON.stringify(Body.vectorGravFo
 console.log("Gravitational force of 3 on 1: " + JSON.stringify(Body.vectorGravForce(myBody3, myBody1)));
 console.log("Gravitational force of 2 on 3: " + JSON.stringify(Body.vectorGravForce(myBody2, myBody3)));
 console.log("Gravitational force of 3 on 2: " + JSON.stringify(Body.vectorGravForce(myBody3, myBody2)));
-
-//Do the above work?
-console.log("Magnitude of Vector Calculation: " + Body.magnitudeOfVector(Body.vectorGravForce(myBody1, myBody2)));
-console.log("Scalar Calculation: " + Body.scalarGravForce(myBody1, myBody2));
 
 //Logging individual net forces
 console.log("Net force of other bodies on " + myBody1.n + ": " + JSON.stringify(Body.netForce(myBody1, myBody2, myBody3)));
